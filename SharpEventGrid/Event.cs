a42eb6c42df2f6fed7125bc4d67c350f73c6eb84
andrecarlucci <andrecarlucci@gmail.com>
2017-10-02 16:16:58 -0300

    adding some docs. Nuget 1.0.2

--- SharpEventGrid/Event.cs
+++ SharpEventGrid/Event.cs
@@ -2,11 +2,29 @@
 
 namespace SharpEventGrid {
     public class Event {
-            public string Id { get; set; } = Guid.NewGuid().ToString();
-            public string EventType { get; set; }
-            public DateTime EventTime { get; set; } = DateTime.Now;
-            public string Topic { get; set; }
-            public string Subject { get; set; }
-            public object Data { get; set; }
-        }
+        /// <summary>
+        /// Unique identifier for the event.
+        /// </summary>
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        /// <summary>
+        /// One of the registered event types for this event source.
+        /// </summary>
+        public string EventType { get; set; }
+        /// <summary>
+        /// The time the event is generated based on the provider's UTC time.
+        /// </summary>
+        public DateTime EventTime { get; set; } = DateTime.Now;
+        /// <summary>
+        /// Full resource path to the event source. This field is not writeable.
+        /// </summary>
+        public string Topic { get; set; }
+        /// <summary>
+        /// Publisher-defined path to the event subject.
+        /// </summary>
+        public string Subject { get; set; }
+        /// <summary>
+        /// Event data specific to the resource provider.
+        /// </summary>
+        public object Data { get; set; }
+    }
 }
